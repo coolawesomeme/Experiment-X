b3b8821fe228ce478f07ed1b4e6b496103dbd022
coolawesomeme <coolawesomeme@gmail.com>
2014-02-01 00:58:19 -0500

    Made random generation a bit better
    Flowers are now 50% as likely, while rocks are 66% as likely

--- src/com/rokru/experiment_x/level/RandomLevel.java
+++ src/com/rokru/experiment_x/level/RandomLevel.java
@@ -13,7 +13,16 @@ public class RandomLevel extends Level {
 	protected void generateLevel() {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				tiles[x + y * width] = random.nextInt(4);
+				int q = random.nextInt(4);
+				if(q == 0){
+					tiles[x + y * width] = q;
+				}else if(q == 1){
+					tiles[x + y * width] = random.nextBoolean() ? q : 0;
+				}else if(q == 2){
+					tiles[x + y * width] = random.nextInt(4) <= 2 ? q : 0;
+				}else{
+					tiles[x + y * width] = q;
+				}
 			}
 		}
 		
