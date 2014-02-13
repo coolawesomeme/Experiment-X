c467e21d4b19ad984969a77538616d5ce49dc690
coolawesomeme <coolawesomeme@gmail.com>
2014-02-12 19:50:29 -0500

    Added starting grass platform
    also, the new rock type

--- src/com/rokru/experiment_x/level/RandomLevel.java
+++ src/com/rokru/experiment_x/level/RandomLevel.java
@@ -15,21 +15,26 @@ public class RandomLevel extends Level {
 	protected void generateLevel() {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				int q = random.nextInt(5);
+				int q = random.nextInt(6);
 				if(q == 0){
-					tiles[x + y * width] = Tile.grass.getTileID();
+					tiles[x + y * height] = Tile.grass.getTileID();
 				}else if(q == 1){
-					tiles[x + y * width] = random.nextInt(4) <= 1 ? Tile.flower_1.getTileID() : 1;
+					tiles[x + y * height] = random.nextInt(5) == 0 ? Tile.flower_1.getTileID() : Tile.grass.getTileID();
 				}else if(q == 2){
-					tiles[x + y * width] = random.nextBoolean() ? Tile.rock.getTileID() : 1;
+					tiles[x + y * height] = random.nextInt(3) == 0 ? Tile.rock_1.getTileID() : Tile.grass.getTileID();
 				}else if(q == 3){
-					tiles[x + y * width] = random.nextInt(4) <= 1 ? Tile.flower_2.getTileID() : 1;
+					tiles[x + y * height] = random.nextInt(5) == 0 ? Tile.flower_2.getTileID() : Tile.grass.getTileID();
+				}else if(q == 4){
+					tiles[x + y * height] = random.nextInt(3) == 0 ? Tile.rock_2.getTileID() : Tile.grass.getTileID();
 				}else{
-					tiles[x + y * width] = Tile.voidTile.getTileID();
+					tiles[x + y * height] = Tile.voidTile.getTileID();
 				}
 			}
 		}
 		tiles[0] = Tile.grass.getTileID();
+		tiles[1] = Tile.grass.getTileID();
+		tiles[128] = Tile.grass.getTileID();
+		tiles[129] = Tile.grass.getTileID();
 	}
 
 }
