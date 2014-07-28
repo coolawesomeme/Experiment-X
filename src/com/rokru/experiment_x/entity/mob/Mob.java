86877d89e1b92c239aa9f72b0c7c3db3c059b143
coolawesomeme <coolawesomeme@gmail.com>
2014-07-28 14:50:12 -0400

    Added collision

--- src/com/rokru/experiment_x/entity/mob/Mob.java
+++ src/com/rokru/experiment_x/entity/mob/Mob.java
@@ -15,22 +15,36 @@ public abstract class Mob extends Entity {
 	protected boolean moving = false;
 			
 	public void move(int xa, int ya) {
+		if(xa != 0 && ya != 0){
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
+		
 		if (xa > 0) dir = 1;
 		if (xa < 0) dir = 2;
 		if (ya > 0) dir = 3;
 		if (ya < 0) dir = 0;
 		
-		if (!collision()) {
+		if (!collision(xa, ya)) {
 			x += xa;
 			y += ya;
+			tileX = x/16;
+			tileY = y/16;
 		}
 	}
 	
 	public void update() {
 	}
 	
-	private boolean collision() {
-		return false;
+	private boolean collision(int xa, int ya) {
+		boolean walkable = true;
+		for(int c = 0; c < 4; c++){
+			int xt = ((x+xa) + c % 2 * 12 - 5) / 16;
+			int yt = ((y+ya) + c / 2 * 12 + 4) / 16;
+			if(!level.getTile(xt, yt).walkable()) walkable = false;
+		}
+		return !walkable;
 	}
 	
 	public void render() {
