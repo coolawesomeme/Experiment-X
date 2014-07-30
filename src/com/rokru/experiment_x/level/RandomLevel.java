4eb82b0c543d5da4d9ffa37f6945e0f715192bbb
coolawesomeme <coolawesomeme@gmail.com>
2014-07-30 16:48:29 -0400

    Added pools or water

--- src/com/rokru/experiment_x/level/RandomLevel.java
+++ src/com/rokru/experiment_x/level/RandomLevel.java
@@ -27,16 +27,38 @@ public class RandomLevel extends Level {
 				}else if(q == 4){
 					tiles[x + y * height] = random.nextInt(5) == 0 ? Tile.rock_2.getTileID() : Tile.grass.getTileID();
 				}else if(x + y * height > 0 && x + y * height < getLevelSide()*getLevelSide()){
-					tiles[x + y * height] = random.nextBoolean() ? Tile.water_1.getTileID() : Tile.water_2.getTileID();
+					tiles[x + y * height] = Tile.grass.getTileID();
 				}else{
 					tiles[x + y * height] = Tile.voidTile.getTileID();
 				}
 			}
 		}
-		tiles[0] = Tile.grass.getTileID();
-		tiles[1] = Tile.grass.getTileID();
-		tiles[128] = Tile.grass.getTileID();
-		tiles[129] = Tile.grass.getTileID();
+		
+		int z = random.nextInt(15);
+		z += 70;
+		
+		System.out.println(z);
+		
+		for(int i = 0; i < z; i++){
+			int px = random.nextInt((width-2));
+			int py = random.nextInt((height-2));
+			
+			if(px <= 2 || py <= 2){
+				px += 3;
+				py += 3;
+			}
+			
+			tiles[px + py * height] = Tile.water_1.getTileID();
+			tiles[(px+1) + py * height] = Tile.water_2.getTileID();
+			tiles[(px-1) + py * height] = Tile.water_2.getTileID();
+			tiles[px + (py+1) * height] = Tile.water_1.getTileID();
+			tiles[px + (py-1) * height] = Tile.water_1.getTileID();
+		}
+		
+		tiles[0 + 0 * height] = Tile.grass.getTileID();
+		tiles[1 + 0 * height] = Tile.grass.getTileID();
+		tiles[0 + 1 * height] = Tile.grass.getTileID();
+		tiles[1 + 1 * height] = Tile.grass.getTileID();
 	}
 
 }
